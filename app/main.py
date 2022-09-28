59c58adc3cd8ce100c2e8a00ca3b728ab09cf091
Thomas Dang <thomas.kyle.dang@gmail.com>
2022-09-28 14:57:45 -0600

    Insert "page" (item) into database working

--- app/main.py
+++ app/main.py
@@ -7,9 +7,33 @@ load_dotenv()
 notion = Client(auth=os.getenv("NOTION_TOKEN"))
 db_id = os.getenv("NOTION_DB_ID")
 
+def create_item(name, completed=False, order=0):
+    notion.pages.create(
+        parent={
+            "database_id": db_id
+        },
+        properties={
+            "Name": {
+                "title": [
+                    {
+                        "text": {
+                            "content": name
+                        }
+                    }
+                ]
+            },
+            "Complete": {
+                "checkbox": completed
+            },
+            "Order": {
+                "number": order
+            }
+        }
+    )
+    
 
 def main():
-    print("Hello World!")
+    create_item("testA", False, 5)
 
 
 if __name__ == '__main__':
