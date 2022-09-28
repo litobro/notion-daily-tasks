d165dd20c34f530c91ee32ce1a567c84e4fe59c4
Thomas Dang <thomas.kyle.dang@gmail.com>
2022-09-28 15:07:36 -0600

    Got basic functionality working of load from file and then send to notion

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 import os
+import time
 from notion_client import Client
 from dotenv import load_dotenv
 
 load_dotenv()
 
-notion = Client(auth=os.getenv("NOTION_TOKEN"))
-db_id = os.getenv("NOTION_DB_ID")
+notion = Client(auth=os.getenv("NOTION_TOKEN")) # Notion API key
+db_id = os.getenv("NOTION_DB_ID") # Notion database ID
 
 def create_item(name, completed=False, order=0):
+    '''Create a new item in the database'''
     notion.pages.create(
         parent={
             "database_id": db_id
@@ -32,8 +34,19 @@ def create_item(name, completed=False, order=0):
     )
     
 
+def load_items():
+    '''Load items from a file'''
+    items = []
+    with open('items.txt', 'r') as f:
+        for line in f:
+            items.append(line.strip())
+    return items
+
+
 def main():
-    create_item("testA", False, 5)
+    for i, item in enumerate(load_items()):
+        create_item(name=item, order=i)
+        time.sleep(0.34) # Notion API rate limit
 
 
 if __name__ == '__main__':
